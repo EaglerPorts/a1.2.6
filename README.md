aca5905f19f2bdcf62ec1d95cef822f1ed7daadb
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-10-23 17:59:36 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Alpha 1.2.6
+<img width="854" height="480" alt="image" src="https://raw.githubusercontent.com/EaglerPorts/a1.2.6/refs/heads/main/img/mainmenu.png" />
---
+++ src/game/java/net/minecraft/src/SoundManager.java
@@ -0,0 +1,49 @@
+package net.minecraft.src;
+
+import net.lax1dude.eaglercraft.internal.vfs2.VFile2;
+
+public class SoundManager {
+	public void func_340_a(GameSettings var1) {
+
+	}
+
+	public void onSoundOptionsChanged() {
+
+	}
+
+	public void closeMinecraft() {
+
+	}
+
+	public void func_6372_a(String var1, VFile2 var2) {
+
+	}
+
+	public void addStreaming(String var1, VFile2 var2) {
+
+	}
+
+	public void addMusic(String var1, VFile2 var2) {
+
+	}
+
+	public void func_4033_c() {
+
+	}
+
+	public void func_338_a(EntityLiving var1, float var2) {
+
+	}
+
+	public void func_331_a(String var1, float var2, float var3, float var4, float var5, float var6) {
+
+	}
+
+	public void func_336_b(String var1, float var2, float var3, float var4, float var5, float var6) {
+
+	}
+
+	public void func_337_a(String var1, float var2, float var3) {
+
+	}
+}
